883bd90855e277a5c7cec1ce452fa5e2ae72562f
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-12-01 20:38:52 +0100

    Introduce a function to react to issue comments
    We will use that to set the reaction to a slash command to the thumbs-up
    (`+1`) upon success, and to `confused` otherwise.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- GitForWindowsHelper/issues.js
+++ GitForWindowsHelper/issues.js
@@ -20,8 +20,22 @@ const getIssueComment = async (context, token, owner, repo, comment_id) => {
     return await sendGitHubAPIRequest(context, token, 'GET', `/repos/${owner}/${repo}/issues/comments/${comment_id}`)
 }
 
+// `reaction` can be one of `+1`, `-1`, `laugh`, `confused`, `heart`, `hooray`, `rocket`, `eyes`
+const createReactionForIssueComment = async (context, token, owner, repo, comment_id, reaction) => {
+    const answer = await sendGitHubAPIRequest(
+        context,
+        token,
+        'POST',
+        `/repos/${owner}/${repo}/issues/comments/${comment_id}/reactions`, {
+            content: reaction
+        }
+    )
+    return answer.id
+}
+
 module.exports = {
     addIssueComment,
     getIssue,
-    getIssueComment
+    getIssueComment,
+    createReactionForIssueComment
 }
