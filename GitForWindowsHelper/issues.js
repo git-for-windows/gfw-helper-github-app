6597d5ea7a179f43cd9a6c0211a05926523f8b56
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-12-01 20:48:03 +0100

    Offer to append text to issue comments
    We will use this to respond to users' slash commands with relevant
    information (e.g. when triggering a workflow run, we will edit the
    comment to contain a link to the run).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- GitForWindowsHelper/issues.js
+++ GitForWindowsHelper/issues.js
@@ -20,6 +20,19 @@ const getIssueComment = async (context, token, owner, repo, comment_id) => {
     return await sendGitHubAPIRequest(context, token, 'GET', `/repos/${owner}/${repo}/issues/comments/${comment_id}`)
 }
 
+const appendToIssueComment = async (context, token, owner, repo, comment_id, append) => {
+    const data = await getIssueComment(context, token, owner, repo, comment_id)
+    const answer = await sendGitHubAPIRequest(
+        context,
+        token,
+        'PATCH',
+        `/repos/${owner}/${repo}/issues/comments/${comment_id}`, {
+            body: `${data.body}${data.body.endsWith('\n\n') ? '' : '\n\n'}${append}`
+        }
+    )
+    return answer.id
+}
+
 // `reaction` can be one of `+1`, `-1`, `laugh`, `confused`, `heart`, `hooray`, `rocket`, `eyes`
 const createReactionForIssueComment = async (context, token, owner, repo, comment_id, reaction) => {
     const answer = await sendGitHubAPIRequest(
@@ -37,5 +50,6 @@ module.exports = {
     addIssueComment,
     getIssue,
     getIssueComment,
+    appendToIssueComment,
     createReactionForIssueComment
 }
