619c0520adb9266ba20e9366e4ad48801e1d0399
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-12-01 23:37:53 +0100

    Support the `/open pr` command
    This command will simply trigger the appropriate GitHub workflow, and
    edit the comment with the command to contain a link to the run.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- GitForWindowsHelper/slash-commands.js
+++ GitForWindowsHelper/slash-commands.js
@@ -4,6 +4,7 @@ module.exports = async (context, req) => {
     const repo = req.body.repository.name
     const issueNumber = req.body.issue.number
     const commenter = req.body.comment.user.login
+    const commentId = req.body.comment.id
 
     if (command === '/hi') {
         const comment = `Hi @${commenter}!`
@@ -15,5 +16,58 @@ module.exports = async (context, req) => {
         await addIssueComment(context, token, owner, repo, issueNumber, comment)
 
         return 'I said hi!'
+    }
+
+    const getToken = (() => {
+        let token
+
+        const get = async () => {
+            const getInstallationIdForRepo = require('./get-installation-id-for-repo')
+            const installationId = await getInstallationIdForRepo(context, owner, repo)
+            const getInstallationAccessToken = require('./get-installation-access-token')
+            return await getInstallationAccessToken(context, installationId)
+        }
+
+        return async () => token || (token = await get())
+    })()
+
+    const checkPermissions = async () => {
+        const getCollaboratorPermissions = require('./get-collaborator-permissions')
+        const token = await getToken()
+        const permission = await getCollaboratorPermissions(context, token, owner, repo, commenter)
+        if (!['ADMIN', 'MAINTAIN', 'WRITE'].includes(permission.toString())) throw new Error(`@${commenter} has no permissions to do that`)
+    }
+
+    try {
+        if (command == '/open pr') {
+            await checkPermissions()
+
+            let [ , package_name, version ] = req.body.issue.title.match(/^\[New (\S+) version\] (\S+)/) || []
+            if (package_name == 'git-lfs') package_name = `mingw-w64-${package_name}`
+            if (version.startsWith('v')) version = version.substring(1)
+            if (!package_name || !version) throw new Error(`Could not parse ${req.issue.title}`)
+
+            const { createReactionForIssueComment } = require('./issues')
+            await createReactionForIssueComment(console, await getToken(), owner, repo, commentId, '+1')
+
+            const triggerWorkflowDispatch = require('./trigger-workflow-dispatch')
+            const answer = await triggerWorkflowDispatch(
+                context,
+                await getToken(),
+                'git-for-windows',
+                'git-for-windows-automation',
+                'open-pr.yml',
+                'main', {
+                    package: package_name,
+                    version
+                }
+            )
+            const { appendToIssueComment } = require('./issues')
+            await appendToIssueComment(context, await getToken(), owner, repo, commentId, `The workflow run [was started](${answer.html_url})`)
+        }
+    } catch (e) {
+        const { createReactionForIssueComment } = require('./issues')
+        await createReactionForIssueComment(console, await getToken(), owner, repo, commentId, 'confused')
+        throw e
     }
 }
