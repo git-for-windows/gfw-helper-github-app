5d92474f3f8acb0d402733f8bd14e329b4a453e1
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-12-01 22:43:43 +0100

    Add a function to dispatch workflow runs
    One of the main building blocks of the "new" Git for Windows package
    deployment process is to let GitHub workflows do most of the job,
    triggered by the GitForWindowsHelper GitHub App.
    
    To this end, the workflows accept a `workflow_dispatch` trigger and this
    here function allows to start those workflow runs.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ GitForWindowsHelper/trigger-workflow-dispatch.js
@@ -0,0 +1,44 @@
+const githubApiRequest = require('./github-api-request')
+
+const sleep = async (milliseconds) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds)
+  })
+}
+
+const getActorForToken = async (context, token) => {
+  const { login } = await githubApiRequest(context, token, 'GET', '/user')
+  return login
+}
+
+const waitForWorkflowRun = async (context, owner, repo, workflow_id, after, token, actor) => {
+  if (!actor) actor = await getActorForToken(context, token)
+  let counter = 0
+  for (;;) {
+    const res = await githubApiRequest(
+      context,
+      token,
+      'GET',
+      `/repos/${owner}/${repo}/actions/runs?actor=${actor}&event=workflow_dispatch&created=>${after}`
+    )
+    const filtered = res.workflow_runs.filter(e => e.path === `.github/workflows/${workflow_id}`)
+    if (filtered.length > 0) return filtered
+    if (counter++ > 30) throw new Error(`Times out waiting for workflow?`)
+    await sleep(1000)
+  }
+}
+
+const triggerWorkflowDispatch = async (context, token, owner, repo, workflow_id, ref, inputs) => {
+  const { headers: { date } } = await githubApiRequest(
+    context,
+    token,
+    'POST',
+    `/repos/${owner}/${repo}/actions/workflows/${workflow_id}/dispatches`,
+    { ref, inputs }
+  )
+
+  const runs = await waitForWorkflowRun(context, owner, repo, workflow_id, new Date(date).toISOString(), token)
+  return runs[0]
+}
+
+module.exports = triggerWorkflowDispatch
