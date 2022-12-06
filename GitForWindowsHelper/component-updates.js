24718f1eef06550edeaa5f8611d9d170568aae07
Johannes Schindelin <johannes.schindelin@gmx.de>
2022-12-06 20:54:41 +0100

    /open pr: add support for PCRE2
    The first attempt at
    https://github.com/git-for-windows/git/issues/4149#issuecomment-1339923548
    triggered a build which failed:
    https://github.com/git-for-windows/git-for-windows-automation/actions/runs/3632938233/jobs/6129363394
    
    There were two problems:
    
    - the package was mistaken for an MSYS one, when we need the MINGW
      variant, and
    
    - the version did not strip the `PCRE2-` prefix from the ticket's title.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- GitForWindowsHelper/component-updates.js
+++ GitForWindowsHelper/component-updates.js
@@ -2,12 +2,12 @@ const guessComponentUpdateDetails = (title) => {
     let [ , package_name, , version ] = title.match(/^\[New (\S+) version\] ([^0-9]+\s+)?(\S+)/) || []
     if (!package_name || !version) throw new Error(`Could not guess component-update details from title '${title}'`)
 
-    if (package_name === 'git-lfs') package_name = `mingw-w64-${package_name}`
+    if (['git-lfs', 'pcre2'].includes(package_name)) package_name = `mingw-w64-${package_name}`
     else if (['gcm-core', 'gcm'].includes(package_name)) package_name = 'mingw-w64-git-credential-manager'
     else if (package_name === 'cygwin') package_name = 'msys2-runtime'
 
     version = version
-        .replace(/^(GCM |openssl-|OpenSSL_|v|V_|GnuTLS |tig-|Heimdal |cyginw-)/, '')
+        .replace(/^(GCM |openssl-|OpenSSL_|v|V_|GnuTLS |tig-|Heimdal |cyginw-|PCRE2-)/, '')
         .replace('_', '.')
         .replace(/-release$/, '')
 
