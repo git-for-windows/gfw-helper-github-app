56ee038928729edbf14d543013e29816ad63d304
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-02-23 00:55:52 +0100

    test-pr-comment-delivery: accept payload via `get-webhook-event-payload.js`
    The output of `get-webhook-event-payload.js` is in a different format
    than when copy/pasting from the web UI of the GitHub App's webhook
    deliveries.
    
    Let's prepare the `test-pr-comment-delivery.js` script to accept its
    input in that format, too.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- test-pr-comment-delivery.js
+++ test-pr-comment-delivery.js
@@ -26,25 +26,35 @@
         headers: {}
     }
 
-    const payloadOffset = contents.indexOf('\n{')
-    if (payloadOffset < 0) throw new Error(`Could not find start of payload in ${path}`)
-    contents.substring(0, payloadOffset).split(/\r?\n/).forEach(line => {
-        const colon = line.indexOf(':')
-        if (colon < 0) return
+    if (contents.startsWith('event: {')) {
+        const event = JSON.parse(contents.substring(7))
+        Object.keys(event.request.headers).forEach(key => {
+            req.headers[key.toLowerCase()] = event.request.headers[key]
+        })
+        req.body = event.request.payload
+        req.rawBody = JSON.stringify(req.body)
+        req.method = 'POST'
+    } else {
+        const payloadOffset = contents.indexOf('\n{')
+        if (payloadOffset < 0) throw new Error(`Could not find start of payload in ${path}`)
+        contents.substring(0, payloadOffset).split(/\r?\n/).forEach(line => {
+            const colon = line.indexOf(':')
+            if (colon < 0) return
 
-        const key = line.substring(0, colon).toLowerCase()
-        const value = line.substring(colon + 1).replace(/^\s+/, '')
+            const key = line.substring(0, colon).toLowerCase()
+            const value = line.substring(colon + 1).replace(/^\s+/, '')
 
-        if (key === 'request method') req.method = value
-        else req.headers[key] = value
-    })
-    req.rawBody = contents.substring(payloadOffset + 1)
-        // In https://github.com/organizations/git-for-windows/settings/apps/gitforwindowshelper/advanced,
-        // the JSON is pretty-printed, but the actual webhook event avoids any
-        // unnecessary white-space in the body
-        .replace(/\r?\n\s*("[^"]*":)\s*/g, '$1')
-        .replace(/\r?\n\s*/g, '')
-    req.body = JSON.parse(req.rawBody)
+            if (key === 'request method') req.method = value
+            else req.headers[key] = value
+        })
+        req.rawBody = contents.substring(payloadOffset + 1)
+            // In https://github.com/organizations/git-for-windows/settings/apps/gitforwindowshelper/advanced,
+            // the JSON is pretty-printed, but the actual webhook event avoids any
+            // unnecessary white-space in the body
+            .replace(/\r?\n\s*("[^"]*":)\s*/g, '$1')
+            .replace(/\r?\n\s*/g, '')
+        req.body = JSON.parse(req.rawBody)
+    }
 
     const context = {
         log: console.log,
