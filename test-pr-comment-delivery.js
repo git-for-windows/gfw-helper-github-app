496cfb1888fee7effe5a150bf65e5982334a5ccc
Johannes Schindelin <johannes.schindelin@gmx.de>
2025-02-13 15:47:45 +0100

    Remove the obsolete code to trigger Azure Pipelines
    Git for Windows has been using GitHub workflows instead, ever since
    https://github.com/git-for-windows/gfw-helper-github-app/pull/29 was
    merged.
    
    Therefore, the code to trigger Azure Pipelines has not been in use for
    almost two years. It's time to retire it.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- test-pr-comment-delivery.js
+++ test-pr-comment-delivery.js
@@ -69,7 +69,6 @@
 
     // avoid accidentally triggering anything
     delete process.env.GITHUB_APP_PRIVATE_KEY
-    delete process.env.AZURE_PIPELINE_TRIGGER_TOKEN
     process.env.DO_NOT_TRIGGER_ANYTHING = 'true'
 
     const index = require('./GitForWindowsHelper/index')
